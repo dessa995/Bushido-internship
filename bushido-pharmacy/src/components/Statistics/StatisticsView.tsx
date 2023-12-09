cc91f860fd3c7d5dd857e93f144f079085939493
dessa995 <dessa995@outlook.com>
2023-12-09 18:16:08 +0100

    bar chart

--- bushido-pharmacy/src/components/Statistics/StatisticsView.tsx
+++ bushido-pharmacy/src/components/Statistics/StatisticsView.tsx
@@ -1,7 +1,63 @@
 import React from 'react';
+import { productDataAtom, manufacturersDataAtom } from '../../App';
+import { useAtom } from 'jotai';
+
+import {
+  BarChart,
+  Bar,
+  Rectangle,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer, // Import ResponsiveContainer
+} from 'recharts';
+
+import styles from './StatisticsView.module.css';
 
 const StatisticsView = () => {
-  return <div>Stats will be here</div>;
+  const [productData] = useAtom(productDataAtom);
+
+  const sortedProductData = [...productData]
+    .sort((a, b) => a.price - b.price)
+    .slice(0.5);
+
+  return (
+    <React.Fragment>
+      <div className={styles.chartsWrapper}>
+        <div className={styles.chartWrapper}>
+          <ResponsiveContainer width="100%" height={300}>
+            {/* Use ResponsiveContainer */}
+            <BarChart
+              data={sortedProductData}
+              margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+              }}
+            >
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey={'name'} />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar
+                dataKey="price"
+                fill="#8884d8"
+                activeBar={<Rectangle fill="pink" stroke="blue" />}
+              />
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+        <div className={styles.chartWrapper}>
+          pie chart
+          {/* https://recharts.org/en-US/examples/TwoSimplePieChart */}
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default StatisticsView;
